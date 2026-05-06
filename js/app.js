48a8b4f1c63e00ec01220f53802f1e74fbf74e32
abhicome <abhicome@gmail.com>
2026-05-06 17:56:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,87 @@
+const root =
+  document.getElementById(
+    "root"
+  );
+
+const state = {
+  tasks: [],
+  projects: []
+};
+
 async function init(){
 
-  root.innerHTML = `
-    <div style="
-      padding:24px;
-      font-size:28px;
-    ">
-      Loading...
-    </div>
-  `;
-
-  await loadAllData();
+  await loadData();
 
   renderHome();
 
 }
 
-function syncData(){
+async function loadData(){
 
-  init();
+  try{
+
+    const tasksRes =
+      await fetch(
+        "./data/work.json?v=99"
+      );
+
+    const projectsRes =
+      await fetch(
+        "./data/projects.json?v=99"
+      );
+
+    state.tasks =
+      await tasksRes.json();
+
+    state.projects =
+      await projectsRes.json();
+
+  }
+
+  catch(err){
+
+    console.log(err);
+
+    state.tasks = [];
+    state.projects = [];
+
+  }
+
+}
+
+function buttonStyle(){
+
+  return `
+    background:#081225;
+    border:none;
+    color:white;
+    padding:14px 20px;
+    border-radius:18px;
+    font-size:18px;
+    cursor:pointer;
+  `;
+
+}
+
+function setToken(){
+
+  const token =
+    prompt(
+      "Enter GitHub Token"
+    );
+
+  if(token){
+
+    localStorage.setItem(
+      "github_token",
+      token
+    );
+
+    alert(
+      "Token Saved"
+    );
+
+  }
 
 }
 
